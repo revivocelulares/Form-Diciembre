9b300c6d861feb50c481f5ae004736e5f6bfe02a
Diego Ferreira <revivocelulares@gmail.com>
2025-11-26 17:44:03 -0300

    update cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,7 +8,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: 'https://inscripciones-dic.vercel.app' }));
+app.use(cors({ origin: 'https://form-diciembre.vercel.app' }));
 app.use(express.json());
 
 app.use("/api", router);
